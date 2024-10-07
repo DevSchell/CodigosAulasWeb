56968e4745fef8eddb29ac6e42335baec5601739
DevSchell <schelldev@gmail.com>
2024-10-07 15:54:21 -0300

    apenas pequenas alterações no código

--- aula5/AtvJavaScript/atv6/script.js
+++ aula5/AtvJavaScript/atv6/script.js
@@ -1,3 +1,11 @@
 let valor1 = prompt("Insira um valor inteiro: ")
 let valor2 = prompt("Insira outro valor inteiro: ")
 
+let resto = valor1%valor2
+
+if ((valor1%valor2) == 0 || (valor2%valor1) == 0){ // TODO
+    alert("Os números são múltiplos")
+} else {
+    alert("Números não são múltiplos")
+}
+
