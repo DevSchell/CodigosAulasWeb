d8bd9d7bbaf4571c10ac87d6d57de246715b6fe9
DevSchell <schelldev@gmail.com>
2024-10-16 10:36:37 -0300

    Layout responsivo
    aula9 - prática com flexbox e layout responsivo. 600x pra mais e 600px pra menos

--- ExerciciosExtras/ToDoList/script.js
+++ ExerciciosExtras/ToDoList/script.js
@@ -1,42 +1,6 @@
 let tasks = [];
 
-function adicionarTarefa(){
-    event.preventDefault();
-    //Validações
-   if (!document.getElementById("nome").value || !document.getElementById("descricao").value) {
-        alert("não pode")
-        return;    
-    }
 
-
-    criarTarefa();
-    resetarInputs();
-    mostrarTarefas();
-}
-
-function resetarInputs(){
-    document.getElementById("nome").value = "";
-    document.getElementById("data").value = "";
-    document.getElementById("prioridade").value = "";
-    document.getElementById("descricao").value = "";
-}
-
-function criarTarefa(){
-    const obj = {
-        nome: document.getElementById("nome").value,
-        //data: document.getElementById("data").value,
-        //prioridade: document.getElementById("propriedade").value,
-        descricao: document.getElementById("descricao").value
-    }
-    tasks.push(obj);
-}
-
-function mostrarTarefas(){
-    let lista = document.getElementById("tarefa-lista");
-    let i = 0;
-    lista.innerHTML = "";
-
-    while (i < tasks.length, i++) {
-        lista.innerHTML += tasks[i].nome + "<br>";
-    }
+function tarefaCriar (){
+    
 }
