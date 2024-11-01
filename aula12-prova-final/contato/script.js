cce3a800d75081920414586848c2faca1b0edbe4
DevSchell <schelldev@gmail.com>
2024-11-01 10:15:42 -0300

    Trabalho final - desenvovimento web
    site portifólio de projetos

---
+++ aula12-prova-final/contato/script.js
@@ -0,0 +1,5 @@
+ function validarContato() {
+    if (!selectProjeto || selectProjeto == "Escolha um projeto") {
+        alert("algo não está certo")
+    }
+ } // Falta melhorar validação
---
+++ aula12-prova-final/login/script.js
@@ -0,0 +1,5 @@
+function validar() {
+    if (document.getElementById("email").value == "admin@admin.com" && document.getElementById("senha").value == "senha") {
+        alert("login realizado com sucesso")
+    }
+}
